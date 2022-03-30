a33415c3b98d80590513e9d83588f320e3413d02
SungThanCong <blueskylove2002@gmail.com>
2022-03-30 19:13:12 +0700

    navigation

--- app/src/main/java/com/hyperlife/MainActivity.java
+++ app/src/main/java/com/hyperlife/MainActivity.java
@@ -1,8 +1,20 @@
 package com.hyperlife;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +22,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+
+        BottomNavigationView btmNav = findViewById(R.id.bottom_nav);
+        btmNav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                handleChangeFragment(item.getItemId());
+                return true;
+            }
+    });
     }
+
+    private void handleChangeFragment(int id) {
+        if(id == R.id.nav_home){
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container,HomeFragment.class,null);
+            fragmentTransaction.commit();
+        }
+        if(id == R.id.nav_gym){
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container,GymFragment.class,null);
+            fragmentTransaction.commit();
+        }
+        if(id == R.id.nav_archie){
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container,UserFragment.class,null);
+            fragmentTransaction.commit();
+        }
+        if(id == R.id.nav_meal){
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container,MealFragment.class,null);
+            fragmentTransaction.commit();
+        }
+    }
+
+
 }
