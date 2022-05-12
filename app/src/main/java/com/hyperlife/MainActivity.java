6985587730918cd329a8c7170976c91d0093d65d
SyLam <lamphusy@gmail.com>
2022-05-12 13:00:48 +0700

    update MealAdapter

--- app/src/main/java/com/hyperlife/MainActivity.java
+++ app/src/main/java/com/hyperlife/MainActivity.java
@@ -69,6 +69,4 @@ public class MainActivity extends AppCompatActivity {
             fragmentTransaction.commit();
         }
     }
-
-
 }
---
+++ app/src/main/java/com/hyperlife/model/MealPlanData.java
@@ -0,0 +1,52 @@
+package com.hyperlife.model;
+
+import android.graphics.drawable.Drawable;
+
+import java.util.ArrayList;
+
+public class MealPlanData {
+    private ArrayList<String> titles;
+    private ArrayList<String> detailOfMeal;
+    private ArrayList<Integer> icons;
+    private ArrayList<Drawable> backgrounds;
+
+    public MealPlanData(ArrayList<String> titles,ArrayList<String> detailOfMeal,
+                        ArrayList<Integer> icons,
+                        ArrayList<Drawable> backgrounds){
+        this.detailOfMeal = detailOfMeal;
+        this.titles = titles;
+        this.icons = icons;
+        this.backgrounds = backgrounds;
+    }
+    public void setIcons(ArrayList<Integer> icons) {
+        this.icons = icons;
+    }
+
+    public void setBackgrounds(ArrayList<Drawable> backgrounds) {
+        this.backgrounds = backgrounds;
+    }
+
+    public void setDetailOfMeal(ArrayList<String> detailOfMeal) {
+        this.detailOfMeal = detailOfMeal;
+    }
+
+    public void setTitles(ArrayList<String> titles) {
+        this.titles = titles;
+    }
+
+    public ArrayList<Drawable> getBackgrounds() {
+        return backgrounds;
+    }
+
+    public ArrayList<Integer> getIcons() {
+        return icons;
+    }
+
+    public ArrayList<String> getDetailOfMeal() {
+        return detailOfMeal;
+    }
+
+    public ArrayList<String> getTitles() {
+        return titles;
+    }
+}
